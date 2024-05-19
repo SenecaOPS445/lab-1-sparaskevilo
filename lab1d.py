cd64b4a3bb2ea38cdb54800ef1a83aa76b2eadfa
Sandra Paraskevilo <sparaskevilo@myseneca.ca>
2024-05-18 21:54:58 -0400

    Added lab1d.py

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Part 3. Complete script with supplied details'''
+#Using variables to allow printout to change if data does
+x = 10
+y = 2
+z = 5
+result = x + y * z
+
+#Converting variables to string to display mathematical operation.
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
